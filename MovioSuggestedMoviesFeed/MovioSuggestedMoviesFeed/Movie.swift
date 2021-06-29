033f9a27bd5029ba7d135eb2889d84586b1f674e
Daniel Torres <danstorre@gmail.com>
2021-06-29 17:55:27 -0300

    created Movie model.

---
+++ MovioSuggestedMoviesFeed/MovioSuggestedMoviesFeed/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  MovioSuggestedMoviesFeed
+//
+//  Created by Daniel Torres on 6/29/21.
+//
+
+import Foundation
+
+struct Movie {
+    let id: UUID
+    let title: String
+    let plot: String
+    let poster: URL?
+}
