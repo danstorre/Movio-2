62a23a6fa8a149bb04c34a511dcafc85945c9bbb
Daniel Torres <danstorre@gmail.com>
2021-06-30 14:38:09 -0300

    check HTTPClient receives the url when load is executed.

--- MovioSuggestedMoviesFeed/MovioSuggestedMoviesFeedTests/RemoteFeedSuggestedMoviesLoaderTests.swift
+++ MovioSuggestedMoviesFeed/MovioSuggestedMoviesFeedTests/RemoteFeedSuggestedMoviesLoaderTests.swift
@@ -9,20 +9,42 @@ class RemoteFeedSuggestedMoviesLoader {
         self.url = url
         self.client = client
     }
+    
+    func load() {
+        client.getDataFrom(url: url)
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    func getDataFrom(url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    func getDataFrom(url: URL) {
+        requestedURL = url
+    }
 }
 
 class RemoteFeedSuggestedMoviesLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let urlToLoad = URL(string: "https://a-url.com")!
-        let httpClient = HTTPClient()
+        let httpClient = HTTPClientSpy()
         let _ = RemoteFeedSuggestedMoviesLoader(url: urlToLoad, client: httpClient)
         
         XCTAssertNil(httpClient.requestedURL)
+    }
+    
+    func test_load_clientExecutesURLFromRemoteLoader() {
+        let urlToLoad = URL(string: "https://a-url.com")!
+        let httpClient = HTTPClientSpy()
+        let remoteLoader = RemoteFeedSuggestedMoviesLoader(url: urlToLoad, client: httpClient)
+        
+        remoteLoader.load()
+        
+        XCTAssertEqual(httpClient.requestedURL, urlToLoad)
     }
 
 }
