475d758fdc3926c0d0f1fc6696d1809fe71cc25a
Daniel Torres <danstorre@gmail.com>
2021-06-30 20:32:53 -0300

    replace stubbing with capturing values.

--- MovioSuggestedMoviesFeed/MovioSuggestedMoviesFeed/Feed API/RemoteFeedSuggestedMoviesLoader.swift
+++ MovioSuggestedMoviesFeed/MovioSuggestedMoviesFeed/Feed API/RemoteFeedSuggestedMoviesLoader.swift
@@ -1,19 +1,25 @@
 import Foundation
 
 public protocol HTTPClient {
-    func getDataFrom(url: URL)
+    func getDataFrom(url: URL, completion: @escaping (Error) -> Void)
 }
 
 public final class RemoteFeedSuggestedMoviesLoader {
     private let url: URL
     private let client: HTTPClient
     
+    public enum Error: Swift.Error {
+        case noConnectivity
+    }
+    
     public init(url: URL, client: HTTPClient) {
         self.url = url
         self.client = client
     }
     
-    public func load() {
-        client.getDataFrom(url: url)
+    public func load(completion: @escaping (Error) -> () = { _ in }) {
+        client.getDataFrom(url: url) { _ in
+            completion(.noConnectivity)
+        }
     }
 }
--- MovioSuggestedMoviesFeed/MovioSuggestedMoviesFeedTests/RemoteFeedSuggestedMoviesLoaderTests.swift
+++ MovioSuggestedMoviesFeed/MovioSuggestedMoviesFeedTests/RemoteFeedSuggestedMoviesLoaderTests.swift
@@ -29,6 +29,18 @@ class RemoteFeedSuggestedMoviesLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
+    func test_load_deliversConnectivityErrorWhenClientFails() {
+        let (sut, client) = makeSUT()
+        
+        var capturedErrors = [RemoteFeedSuggestedMoviesLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        let clientError = NSError(domain: "error", code: 400, userInfo: nil)
+        client.completesWithError(error: clientError)
+        
+        XCTAssertEqual(capturedErrors, [.noConnectivity])
+    }
+    
     // Mark: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedSuggestedMoviesLoader, client: HTTPClientSpy) {
@@ -39,9 +51,15 @@ class RemoteFeedSuggestedMoviesLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
+        var completions = [((Error) -> Void)]()
         
-        func getDataFrom(url: URL) {
+        func getDataFrom(url: URL, completion: @escaping (Error) -> Void) {
+            completions.append(completion)
             requestedURLs.append(url)
+        }
+        
+        func completesWithError(error: NSError, at index: Int = 0) {
+            completions[index](error)
         }
     }
 }
