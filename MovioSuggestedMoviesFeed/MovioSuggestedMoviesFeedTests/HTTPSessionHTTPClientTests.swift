c32a76833e0558bde85f3fbebfb5c446b52833de
Daniel Torres <danstorre@gmail.com>
2021-08-05 12:31:15 -0300

    test all invalid cases.

--- MovioSuggestedMoviesFeed/MovioSuggestedMoviesFeedTests/HTTPSessionHTTPClientTests.swift
+++ MovioSuggestedMoviesFeed/MovioSuggestedMoviesFeedTests/HTTPSessionHTTPClientTests.swift
@@ -62,8 +62,21 @@ class HTTPSessionHTTPClientTests: XCTestCase {
         XCTAssertEqual(receivedError?.code, clientError.code)
     }
     
-    func test_getFromURL_deliversFailureWhenAllValuesAreNil() {
+    func test_getFromURL_deliversFailureWhenAllInvalidCases() {
+        let nonHTTPURLResponse = URLResponse(url: anyURL(), mimeType: nil, expectedContentLength: 0, textEncodingName: nil)
+        let anyHTTPURLResponse = HTTPURLResponse(url: anyURL(), statusCode: 200, httpVersion: nil, headerFields: nil)
+        let anyData = Data("any data".utf8)
+        let anyError = NSError(domain: "a domain error", code: 1, userInfo: nil)
         XCTAssertNotNil(resultErrorFor(data: nil, response: nil, error: nil))
+        XCTAssertNotNil(resultErrorFor(data: nil, response: nonHTTPURLResponse, error: nil))
+        XCTAssertNotNil(resultErrorFor(data: nil, response: anyHTTPURLResponse, error: nil))
+        XCTAssertNotNil(resultErrorFor(data: anyData, response: nil, error: nil))
+        XCTAssertNotNil(resultErrorFor(data: anyData, response: nil, error: anyError))
+        XCTAssertNotNil(resultErrorFor(data: nil, response: nonHTTPURLResponse, error: anyError))
+        XCTAssertNotNil(resultErrorFor(data: nil, response: anyHTTPURLResponse, error: anyError))
+        XCTAssertNotNil(resultErrorFor(data: anyData, response: nonHTTPURLResponse, error: anyError))
+        XCTAssertNotNil(resultErrorFor(data: anyData, response: anyHTTPURLResponse, error: anyError))
+        XCTAssertNotNil(resultErrorFor(data: anyData, response: nonHTTPURLResponse, error: nil))
     }
     
     // MARK: - Helper Methods
